586ef57509080ccda4eae55131e80310bda0ba10
Will Schwier <w.schwier@hotmail.co.uk>
2017-05-02 11:46:26 +0100

    rails shit

--- yelp_clone/app/controllers/restaurants_controller.rb
+++ yelp_clone/app/controllers/restaurants_controller.rb
@@ -1,6 +1,6 @@
 class RestaurantsController < ApplicationController
-  def index
-    raise 'Hello from the index action'
-  end
-  
+  # def index
+  #   raise 'Hello from the index action'
+  # end
+  #
 end
