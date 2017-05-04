a5ec6b289ad7b8136dfba00899748c4ed4f87b5a
Edward Ward <edwardwardward@gmail.com>
2017-05-04 15:29:00 +0100

    Tests update

--- yelp_clone/spec/features/restaurants_feature_spec.rb
+++ yelp_clone/spec/features/restaurants_feature_spec.rb
@@ -3,12 +3,7 @@ require 'rails_helper'
 feature 'restaurants' do
 
   before do
-    visit('/')
-    click_link('Sign up')
-    fill_in('Email', with: 'test@example.com')
-    fill_in('Password', with: 'testtest')
-    fill_in('Password confirmation', with: 'testtest')
-    click_button('Sign up')
+    sign_up
   end
 
   context 'no restaurants have been added' do
@@ -22,11 +17,8 @@ feature 'restaurants' do
   context 'restaurants have been added' do
 
     before do
-      visit '/restaurants'
-      click_link 'Add a restaurant'
-      fill_in 'Name', with: 'KFC'
-      click_button 'Create Restaurant'
-      click_link 'Sign out'
+      create_restaurant
+      sign_out
     end
 
     scenario 'display restaurants' do
@@ -38,20 +30,14 @@ feature 'restaurants' do
 
   context 'creating restaurants' do
     scenario 'prompts user to fill out a form, then displays the new restaurant' do
-      visit '/restaurants'
-      click_link 'Add a restaurant'
-      fill_in 'Name', with: 'KFC'
-      click_button 'Create Restaurant'
+      create_restaurant
       expect(page).to have_content 'KFC'
       expect(current_path).to eq '/restaurants'
     end
 
     context 'an invalid restaurant' do
       scenario 'does not let you submit a name that is too short' do
-        visit '/restaurants'
-        click_link 'Add a restaurant'
-        fill_in 'Name', with: 'kf'
-        click_button 'Create Restaurant'
+        create_restaurant(name: 'kf')
         expect(page).not_to have_css 'h2', text: 'kf'
         expect(page).to have_content 'error'
       end
@@ -61,25 +47,22 @@ feature 'restaurants' do
   context 'viewing restaurants' do
 
     before do
-      visit '/restaurants'
-      click_link 'Add a restaurant'
-      fill_in 'Name', with: 'KFC'
-      click_button 'Create Restaurant'
-      click_link 'Sign out'
+      create_restaurant
+      sign_out
     end
 
     scenario 'lets a user view a restaurant' do
      visit '/restaurants'
      click_link 'KFC'
      expect(page).to have_content 'KFC'
-     expect(current_path).to eq "/restaurants/#{kfc.id}"
     end
   end
 
   context 'editing restaurants' do
-    before { Restaurant.create name: 'KFC', description: 'Deep fried goodness', id: 1}
+
 
     scenario 'let a user edit a restaurant' do
+      create_restaurant
       visit '/restaurants'
       click_link 'Edit KFC'
       fill_in 'Name', with: 'Kentucky Fried Chicken'
@@ -88,7 +71,6 @@ feature 'restaurants' do
       click_link 'Kentucky Fried Chicken'
       expect(page).to have_content 'Kentucky Fried Chicken'
       expect(page).to have_content 'Deep fried goodness'
-      expect(current_path).to eq '/restaurants/1'
     end
 
   end
@@ -96,11 +78,7 @@ feature 'restaurants' do
   context 'deleting restaurants' do
 
     scenario 'removes a restaurant when a user clicks a delete link' do
-      visit '/restaurants'
-      click_link 'Add a restaurant'
-      fill_in 'Name', with: 'KFC'
-      click_button 'Create Restaurant'
-
+      create_restaurant
       visit '/restaurants'
       click_link 'Delete KFC'
       expect(page).to have_content 'KFC deleted successfully'
@@ -111,18 +89,9 @@ feature 'restaurants' do
   context 'user permissions' do
 
     before do
-      visit '/restaurants'
-      click_link 'Add a restaurant'
-      fill_in 'Name', with: 'KFC'
-      click_button 'Create Restaurant'
-      click_link 'Sign out'
-
-      visit('/')
-      click_link('Sign up')
-      fill_in('Email', with: 'test2@example.com')
-      fill_in('Password', with: 'testtest2')
-      fill_in('Password confirmation', with: 'testtest2')
-      click_button('Sign up')
+      create_restaurant
+      sign_out
+      sign_up(email: 'test2@example.com',password: 'testtest2')
     end
 
     scenario 'user cannot edit restaurant' do
--- yelp_clone/spec/models/restaurant_spec.rb
+++ yelp_clone/spec/models/restaurant_spec.rb
@@ -8,8 +8,8 @@ describe Restaurant, type: :model do
   end
 
   it "is not valid unless it has a unique name" do
-    Restaurant.create(name: "Moe's Tavern")
-    restaurant = Restaurant.new(name: "Moe's Tavern")
+    Restaurant.create(name: "Moe's Tavern",user: User.create)
+    restaurant = Restaurant.create(name: "Moe's Tavern",user: User.create)
     expect(restaurant).to have(1).error_on(:name)
   end
 end
