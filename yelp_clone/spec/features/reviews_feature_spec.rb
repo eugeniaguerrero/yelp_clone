4739fa6453967f7dc66013ff60389112fecbc964
Will Schwier <w.schwier@hotmail.co.uk>
2017-05-04 12:40:32 +0100

    Add user email to review

--- yelp_clone/spec/features/reviews_feature_spec.rb
+++ yelp_clone/spec/features/reviews_feature_spec.rb
@@ -3,6 +3,15 @@ require 'rails_helper'
 feature 'reviewing' do
   before { Restaurant.create name: 'KFC' }
 
+  before do
+    visit '/restaurants'
+    click_link 'Sign up'
+    fill_in 'Email', with: "will@will.will"
+    fill_in 'Password', with: "will123"
+    fill_in 'Password confirmation', with: "will123"
+    click_button 'Sign up'
+  end
+
   scenario 'allows users to leave a review using a form' do
      visit '/restaurants'
      click_link 'Review KFC'
